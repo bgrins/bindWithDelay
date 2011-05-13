e23c2502458c516e24dd0b16edcde8b2f1636f3f
bgrins <briangrinstead@gmail.com>
2011-05-13 18:55:56 -0500

    add ability to throttle (allow events to come through every n milliseconds rather than wait until all have stopped for that long)

--- bindWithDelay.js
+++ bindWithDelay.js
@@ -6,31 +6,35 @@ Open source code under MIT license: http://www.opensource.org/licenses/mit-licen
 
 Usage: 
 	See http://api.jquery.com/bind/
-	.bindWithDelay( eventType, [ eventData ], handler(eventObject), timeout )
+	.bindWithDelay( eventType, [ eventData ], handler(eventObject), timeout, throttle )
 
 Examples:
 	$("#foo").bindWithDelay("click", function(e) { }, 100);
 	$(window).bindWithDelay("resize", { optional: "eventData" }, callback, 1000);
+	$(window).bindWithDelay("resize", callback, 1000, true);
 */
 
 (function($) {
-$.fn.bindWithDelay = function( type, data, fn, timeout ) {
+$.fn.bindWithDelay = function( type, data, fn, timeout, throttle ) {
 	var wait = null;
 	var that = this;
 	
 	if ( $.isFunction( data ) ) {
+		throttle = timeout;
 		timeout = fn;
 		fn = data;
 		data = undefined;
 	}
+	
+	function cb() {
+		var e = $.extend(true, { }, arguments[0]);
+		var throttler = function() {
+			wait = null;
+			fn.apply(that, [e]);
+		};
 		
-	var cb = function(e) {
-		clearTimeout(wait);
-		var cachedEvent = $.extend({}, e);
-		
-		wait = setTimeout(function() {
-			fn.apply(that, [cachedEvent]);
-		}, timeout);
+		if (!throttle) { clearTimeout(wait); }
+		if (!throttle || !wait) { wait = setTimeout(throttler, timeout); }
 	}
 	
 	return this.bind(type, data, cb);
